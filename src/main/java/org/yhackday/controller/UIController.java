ac9cba8426cd0c386eaf16dd00d7d371c2e8ed1f
urashin <s.urakawa@hotmail.co.jp>
2021-03-21 05:48:33 +0900

    add html

---
+++ src/main/java/org/yhackday/controller/UIController.java
@@ -0,0 +1,18 @@
+package org.yhackday.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin
+@Controller
+@RequestMapping("/dummy")
+public class UIController {
+    @GetMapping("/action/{userId}")
+    public String getNextActionInfo(@PathVariable int userId) {
+        return "action_ui";
+    }
+}
