e8a8343b32b7f4e2f877da484fabf5884fe86afe
urashin <s.urakawa@hotmail.co.jp>
2021-03-21 12:02:04 +0900

    fix ui

--- src/main/java/org/yhackday/controller/UIController.java
+++ src/main/java/org/yhackday/controller/UIController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @CrossOrigin
 @Controller
-@RequestMapping("/dummy")
+@RequestMapping("/v1")
 public class UIController {
     @GetMapping("/action/{userId}")
     public String getNextActionInfo(@PathVariable int userId) {
